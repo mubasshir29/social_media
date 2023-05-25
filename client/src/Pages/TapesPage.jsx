6e96b6b41f633826782b01a374649c38f9fe969d
mubasshir29 <mubasshir.f29@gmail.com>
2023-05-25 06:42:41 +0400

    started Tapes module

--- client/src/Pages/TapesPage.jsx
+++ client/src/Pages/TapesPage.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
+import tapeData from './../Data/tapeData.json'
 
 function TapesPage() {
   return (
-    <div>TapesPage</div>
+    <div className='w-[92%] mx-auto'>
+      <h2 className='text-4xl font-bold py-3'>Tapes</h2>
+      <div className='flex flex-col gap-4'>
+      <div id="trending" className='flex flex-col gap-3'>
+        <h3 className='font-bold'>Trending</h3>
+        <div className='w-full flex border-t-4 py-3 gap-2 overflow-x-auto scrollbar-hide'>
+          {tapeData.map((item,index) =><img className='w-36 rounded-lg' src={item.tape_thumbnail} />)}
+        </div>
+      </div>
+      <div id="recommended" className='flex flex-col gap-2'>
+        <h3 className='font-bold'>Recommended</h3>
+        <div className='w-full flex border-t-4 py-3 gap-2 overflow-x-auto scrollbar-hide'>
+          {tapeData.map((item,index) => <img className='w-36 rounded-lg' src={item.tape_thumbnail} />)}
+        </div>
+      </div>
+      <div id="all" className='flex flex-col gap-3'>
+        <div className='w-full flex border-t-4 py-3 gap-2 overflow-x-auto scrollbar-hide'>
+          {tapeData.map((item,index) => <img className='w-36 rounded-lg' src={item.tape_thumbnail} />)}
+        </div>
+      </div>
+      </div>
+     
+    </div>
+    
   )
 }
 
